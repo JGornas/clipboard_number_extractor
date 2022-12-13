9c5a9f8127904f606e31b4f7198dca74a9f748a7
Jake <jjgornas@gmail.com>
2022-12-13 19:53:42 +0100

    Refactor

--- clipboard_number_extractor.py
+++ clipboard_number_extractor.py
@@ -5,31 +5,30 @@ import clipboard
 
 class Extractor:
     def __init__(self):
-        self.ENHANCE_FACTOR = 1.5
         self.image = None
         self.numbers = None
 
     def __str__(self):
-        return f"Numbers found:\n{self.numbers}" if len(self.numbers) > 0 else "No numbers found."
+        return f"Numbers found:\n{self.numbers}" if self.numbers else "No numbers found."
 
-    def enhance_image(self):
-        # Increase contrast and sharpness of self.image
+    def enhance_image(self, enhance_factor=1.5):
+        """ Increase contrast and sharpness of self.image """
         contrast_enhancer = ImageEnhance.Contrast(self.image)
-        self.image = contrast_enhancer.enhance(self.ENHANCE_FACTOR)
+        self.image = contrast_enhancer.enhance(enhance_factor)
         sharpness_enhancer = ImageEnhance.Contrast(self.image)
-        self.image = sharpness_enhancer.enhance(self.ENHANCE_FACTOR)
+        self.image = sharpness_enhancer.enhance(enhance_factor)
 
     def parse_image(self):
-        # Analyze image and extract numbers
+        """ Analyze image and extract numbers """
         pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
         self.numbers = image_to_string(self.image, lang="eng", config="digits").strip()
 
     def get_clipboard(self):
-        # Get the clipboard content
+        """ Get the clipboard content """
         self.image = ImageGrab.grabclipboard()
 
     def set_clipboard(self):
-        # Set the clipboard content
+        """ Set the clipboard content """
         clipboard.copy(self.numbers)
 
 
@@ -41,10 +40,8 @@ def main():
         extractor.parse_image()
         extractor.set_clipboard()
         print(extractor)
-
-    # The clipboard content is not an image
-    except Exception as e:
-        print(f"Wrong content in the clipboard! Error found: {e}")
+    except AttributeError:
+        print("No image in clipboard.")
 
 
 if __name__ == "__main__":
