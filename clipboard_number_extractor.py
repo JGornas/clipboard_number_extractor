516642ce3d1cc1ad119706bc8155d886015cadd5
Jake <jjgornas@gmail.com>
2022-12-13 15:56:12 +0100

    Changed main function into Extractor class. Main initiates extractor and runs all extractor  methods in order.

--- clipboard_number_extractor.py
+++ clipboard_number_extractor.py
@@ -1,32 +1,50 @@
 from PIL import ImageGrab, ImageEnhance
 from pytesseract import image_to_string, pytesseract
+import clipboard
+
+
+class Extractor:
+    def __init__(self):
+        self.ENHANCE_FACTOR = 1.5
+        self.image = None
+        self.numbers = None
+
+    def __str__(self):
+        return f"Numbers found:\n{self.numbers}" if len(self.numbers) > 0 else "No numbers found."
+
+    def enhance_image(self):
+        # Increase contrast and sharpness of self.image
+        contrast_enhancer = ImageEnhance.Contrast(self.image)
+        self.image = contrast_enhancer.enhance(self.ENHANCE_FACTOR)
+        sharpness_enhancer = ImageEnhance.Contrast(self.image)
+        self.image = sharpness_enhancer.enhance(self.ENHANCE_FACTOR)
+
+    def parse_image(self):
+        # Analyze image and extract numbers
+        pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        self.numbers = image_to_string(self.image, lang="eng", config="digits").strip()
+
+    def get_clipboard(self):
+        # Get the clipboard content
+        self.image = ImageGrab.grabclipboard()
+
+    def set_clipboard(self):
+        # Set the clipboard content
+        clipboard.copy(self.numbers)
 
 
 def main():
-    # Check if the clipboard content is an image
     try:
-        # Get the clipboard content
-        image = ImageGrab.grabclipboard()
-        image.save('clipboard.png', 'PNG')
-
-        # Increase contrast and sharpness
-        factor = 1.5
-        contrast_enhancer = ImageEnhance.Contrast(image)
-        image_output = contrast_enhancer.enhance(factor)
-        sharpness_enhancer = ImageEnhance.Contrast(image_output)
-        image_output = sharpness_enhancer.enhance(factor)
-
-        # Parse image with tesseract
-        pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-        numbers = image_to_string(image_output, lang="eng", config="digits")
-
-        # Print the numbers
-        print(f"Numbers found:\n{numbers}" if len(numbers) > 0 else "No numbers found")
+        extractor = Extractor()
+        extractor.get_clipboard()
+        extractor.enhance_image()
+        extractor.parse_image()
+        extractor.set_clipboard()
+        print(extractor)
 
     # The clipboard content is not an image
     except Exception as e:
-        print(f"No file in clipboard! Error found: {e}")
-        exit()
+        print(f"Wrong content in the clipboard! Error found: {e}")
 
 
 if __name__ == "__main__":
